128c016c73c09e0e7b1548d73076dd815ec9c33e
PazMauro <132943783+PazMauro@users.noreply.github.com>
2025-04-10 15:49:05 -0300

    Update TP.java

--- TP.java
+++ TP.java
@@ -15,46 +15,52 @@ public class Main {
 			
 		}while(opcion != 11);
 		
-	
-		
-	
 	}
 	
 	public static int mostrarMenuYElegirOpcion () {
 		int opcion;
 			
 		
-			System.out.println("--------------------------------------------------------------");
-			System.out.println("(1) agregar juego");
-			System.out.println("(2)  consultar juego");
-			System.out.println("(3)  modificar juego");
-			System.out.println("(4) eliminar juego");
-			System.out.println("(5) listar todos los juego");
-			System.out.println("(6) buscar juegos por genero");
-			System.out.println("(7) buscar juegos por desarrollador");
-			System.out.println("(8) buscar juegos mejor/peor calificados");
-			System.out.println("(9) buscar juegos por rango de precios");
-			System.out.println("(10) calcular estadisticas de la plataforma");
-			System.out.println("(11) salir");
+			System.out.println("------------------------ Menú --------------------------------");
+			System.out.println("(1) Agregar juego");
+			System.out.println("(2) Consultar juego");
+			System.out.println("(3) Modificar juego");
+			System.out.println("(4) Eliminar juego");
+			System.out.println("(5) Listar todos los juego");
+			System.out.println("(6) Buscar juegos por genero");
+			System.out.println("(7) Buscar juegos por desarrollador");
+			System.out.println("(8) Buscar juegos mejor/peor calificados");
+			System.out.println("(9) Buscar juegos por rango de precios");
+			System.out.println("(10) Calcular estadisticas de la plataforma");
+			System.out.println("(11) Salir");
 			System.out.println("--------------------------------------------------------------");
 
 			Scanner S = new Scanner (System.in);
 			System.out.println("ingrese una opcion: ");	
 			opcion = S.nextInt();	
+			
 		return opcion;
 	}
+	
 	public static int ingresarJuego(String juegos[][],int i) {
 		Scanner S = new Scanner (System.in);
 		
 	
-		int id,precio,calificacion,genero,anio,clasificacion;
+		int id;
+		int precio;
+		int calificacion;
+		int genero;
+		int anio;
+		int clasificacion;
+		
 		System.out.println("ingrese id del juego dentro del rango 1000 a 9999: " + i);
-		juegos[i][0] = S.nextLine();
-
+		id = S.nextInt();
+		juegos[i][0] = String.valueOf(id);
+		S.nextLine();
 		
 		System.out.println("ingrese titulo del juego: ");
 		juegos[i][1] = S.nextLine();
-
+		
 		
 		System.out.println("ingrese desarrolador del juego: ");
 		juegos[i][2] = S.nextLine();
@@ -63,29 +69,34 @@ public class Main {
 		System.out.println("ingrese genero del juego: ");
 		genero = S.nextInt();
 		juegos[i][3] = String.valueOf(genero);
-
+		S.nextLine();
 		
 		System.out.println("ingrese clasificacion del juego: ");
 		clasificacion = S.nextInt();
 		juegos[i][4] = String.valueOf(clasificacion);
-
+		S.nextLine();
 		
 		System.out.println("ingrese año de lanzamiento del juego: ");
 		anio = S.nextInt();
 		juegos[i][5] = String.valueOf(anio);
-	
+		S.nextLine();
 		
 		System.out.println("ingrese el precio del juego: ");
 		precio = S.nextInt();
 		juegos[i][6] = String.valueOf(precio);
-		
+		S.nextLine();
 		
 		System.out.println("ingrese la calificacion del juego: ");
 		calificacion = S.nextInt();
 		juegos[i][7] = String.valueOf(calificacion);
-	
+		S.nextLine();
+		
 		i++;
-	return i;
+	
+		
+		
+		return i;
+		
 	}
 	
 	public static int generarAccion (int opcion,String juegos[][],int i) {
@@ -135,12 +146,25 @@ public class Main {
 	}
 			return i;
 
-
-
-		
+			
 	}
+	
+	
 	public static void ingresarEntero (String juegos[][]) {
+		int minid;
+		int maxid;
 		
+		minid=1000;
+		maxid=9999;
+		
+		boolean error = false;
+		
+		do {
+			
+			
+			
+			
+		}while(error);
 		
 	}
 
